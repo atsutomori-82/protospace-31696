a497e8a488f9134416ea9ef3b57073523ed15849
atsutomori-82 <atsutomori.82@gmail.com>
2020-11-14 14:55:13 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,11 @@
+class Prototype < ApplicationRecord
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :concept, presence: true
+  validates :catch_copy, presence: true
+  validates :image, presence: true
+
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+end
